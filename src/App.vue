b4e776c074e8f3d4be7c01bd294c28d69bcee81a
Ferdinan Kurnia <ferdinankurnian@gmail.com>
2025-01-29 05:59:16 +0700

    recreate with bun

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="animate-bounce text-6xl text-emerald-500 font-mono">
+    You're Awesome! 🚀
+  </div>
+  <div>
+    <span class="material-symbols-outlined text-4xl text-blue-500">
+      home
+    </span>
+    <p class="mt-2">Ini icon rumah!</p>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    // theme: {...}, // Opsional di v4 (jika ingin custom)
+    plugins: [],
+  }
